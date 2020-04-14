aba298e863b6fa97de5b12c398190bb65a88b850
myk245 <myk245@nyu.edu>
2020-04-13 23:53:51 -0400

    render outfits page

--- src/Components/OutfitCard.js
+++ src/Components/OutfitCard.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Card, CardGroup, CardTitle } from 'react-bootstrap';
+import { Container, Row, Col} from 'react-bootstrap';
 
-const OutfitCard = () => {
-    return <h1>OutfitCard!</h1>
+
+class OutfitCard extends React.Component {
+
+    render() {
+        const outfit = this.props.outfit
+        const top = this.props.outfit.top
+        const user = this.props.outfit.user
+        
+        return (
+            <Container>
+                <Container><strong>{outfit.name}</strong></Container>
+                <Row>
+                    <Col>
+                            <img className="card-img-top" src={top.img_url} alt="Card image cap" />
+                            <div className="card-body">
+                                <h6 className="card-title">{top.name}</h6>
+                            </div>
+                    </Col>
+                    <Col>
+                            <img className="card-img-top" src={top.img_url} alt="Card image cap" />
+                            <div className="card-body">
+                                <h6 className="card-title">{top.name}</h6>
+                            </div>
+                    </Col>
+                    <Col>
+                            <img className="card-img-top" src={top.img_url} alt="Card image cap" />
+                            <div className="card-body">
+                                <h6 className="card-title">{top.name}</h6>
+                            </div>
+                    </Col>
+                </Row> 
+                <br></br>
+            </Container>
+        )
+    }
 }
 
-export default OutfitCard
+export default OutfitCard; 
