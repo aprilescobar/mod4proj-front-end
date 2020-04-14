bf1ead49807d3e1ce8b74a01a23bb06cff054fb0
aprilfaithdays <aprilfaith.e@gmail.com>
2020-04-14 13:04:22 -0400

    updated links

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -2,15 +2,25 @@ import React from 'react';
 import Button from 'react-bootstrap/Button'
 
 const ProductCard = props => {
-
     const product = props.product
+
+    const handleClick = () => {
+        props.getId(product.id)
+    }
+
     return (
         <div className="productCards">
-            <div class="gallery">
+            <div className="gallery">
                     <img src={product.img_url} />
-                <div class="desc">
+                <div className="desc">
                     <h6>{product.name}</h6>
-                    <Button variant="outline-secondary" value={product.id}>+ Collection</Button>{' '}
+                    <Button 
+                        variant="outline-secondary" 
+                        value={product.id}
+                        onClick={handleClick}
+                    >
+                        + Collection
+                    </Button>{' '}
                 </div>
             </div>
         </div>
